43d77bb74b237d067955869487a4227190fae550
t6meja00 <t6meja00@students.oamk.fi>
2017-11-21 11:29:35 +0200

    testi

--- testi2/Form1.Designer.cs
+++ testi2/Form1.Designer.cs
@@ -276,7 +276,6 @@
             this.button3.TabIndex = 8;
             this.button3.Text = "close";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
