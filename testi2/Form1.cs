826bc1b0a6fe13493080b25b078f751ba84a6749
t6meja00 <t6meja00@students.oamk.fi>
2017-11-29 08:35:00 +0200

    Delete date selected from calendar

--- testi2/Form1.cs
+++ testi2/Form1.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,7 @@ namespace testi2
     {
         private int hours = DateTime.Now.Hour;
         private int minutes = DateTime.Now.Minute;
-        
+
 
         public Form1()
         {
