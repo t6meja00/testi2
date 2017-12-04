d5a5791ab2cd82e175bd94db157334e85755c1d1
t6meja00 <t6meja00@students.oamk.fi>
2017-12-04 14:11:09 +0200

    Revert "Delete date selected from calendar"
    This reverts commit 826bc1b0a6fe13493080b25b078f751ba84a6749.

--- testi2/Admin.cs
+++ testi2/Admin.cs
@@ -11,22 +11,10 @@ namespace testi2
     {
         private void ButtonEmpty_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == System.Windows.Forms.DialogResult.Yes)
+            if (MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes)
             {
                 Database.Execute("truncate table weather");
             }
-        }
-
-        private void CleanButton_Click(object sender, EventArgs e)
-        {
-            Database.Execute("DELETE FROM weather WHERE humidity > 80");
-            Database.Execute("DELETE FROM weather WHERE temperature > 80 OR temperature < -20");
-        }
-        private void SelectedButton_Click(object sender, EventArgs e)
-        {
-            string date = monthCalendar1.SelectionRange.Start.ToString("yyyy-MM-dd");
-            Database.Execute("Delete FROM weather WHERE time LIKE '%"+date+"%'");
-            Console.WriteLine(date);
         }
     }
 }
